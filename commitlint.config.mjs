1fa679b71a0ef2e6a90a38ec1d58d28570bf9e29
unional <homawong@gmail.com>
2026-01-23 22:49:12 -0800

    chore: update commitlint config

---
+++ commitlint.config.mjs
@@ -0,0 +1,11 @@
+const DISABLE = [0, 'always']
+
+export default {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'subject-case': DISABLE,
+		'header-max-length': DISABLE,
+		'body-max-line-length': DISABLE,
+		'footer-max-line-length': DISABLE,
+	},
+}
